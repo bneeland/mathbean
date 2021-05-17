7f727e0ea2ffb6f80fa35550132be989d3e9bb2b
Brian Neeland <brian.neeland@protonmail.com>
2021-05-16 20:41:32 -0600

    Got vue to save fabric edits through axios to django rest framework

--- hybrid_app/views.py
+++ hybrid_app/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from django.views.generic.list import ListView
 from django.views.generic.base import TemplateView
 from django.views.generic.base import RedirectView
