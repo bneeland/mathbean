4a5734f3d5d9254e4bdb8604c28980e9ea68a2c0
Brian Neeland <brian.neeland@protonmail.com>
2021-06-18 22:37:36 -0600

    Renamed project to mathbean

--- mathbean/urls.py
+++ mathbean/urls.py
@@ -1,4 +1,4 @@
-"""hybrid_project URL Configuration
+"""mathbean URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
